223e047891aeed91faa9b2dca43de31f98cf4378
Barrilli01 <gabrielbarrillicruz@gmail.com>
2023-09-13 17:30:54 -0300

    Navbar Styling

--- frontweb/src/components/Navbar/index.tsx
+++ frontweb/src/components/Navbar/index.tsx
@@ -1,20 +1,42 @@
+import './styles.css';
+import 'bootstrap/js/src/collapse.js';
+
 const Navbar = () => {
   return (
-    <nav className="bg-primary">
-      <div>
-        <a href="link">
+    <nav className="navbar navbar-expand-md navbar-dark bg-primary main-nav">
+      <div className="container-fluid">
+        <a href="link" className="nav-logo-text">
           <h4>Ds Catalogy</h4>
         </a>
-        <div>
-          <ul>
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#dscatalog-navbar"
+          aria-controls="dscatalog-navbar"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="dscatalog-navbar">
+          <ul className="navbar-nav offset-md-2 main-menu">
             <li>
-              <a href="link">Home</a>
+              <a href="link" className="active">
+                Home
+              </a>
             </li>
             <li>
-              <a href="link">Catálogo</a>
+              <a href="link" className="active">
+                Catálogo
+              </a>
             </li>
             <li>
-              <a href="link">Admin</a>
+              <a href="link" className="active">
+                Admin
+              </a>
             </li>
           </ul>
         </div>
